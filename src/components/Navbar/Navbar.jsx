7e557c4dc7220f8883c693995d29efe4aeadb33e
DevMarioNan <80198355+DevMarioNan@users.noreply.github.com>
2023-01-31 16:46:23 +0200

    signout functionallity added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React,{useContext} from 'react'
 import { Link } from 'react-router-dom'
 import {AuthContext} from '../../features/context/Auth'
-
+import {auth} from '../../utils/firebase/firebase'
 
 const Navbar = () => {
     const {currentUser} = useContext(AuthContext)
@@ -22,7 +22,10 @@ const Navbar = () => {
                     <h1 className='text-xl font-mono cursor-pointer ml-5'>SignIn</h1>
                 </Link> 
                 : 
+                <div className='flex items-center'>
                 <h1 className='text-xl font-mono cursor-pointer ml-5'>Hi, {currentUser.email}</h1>
+                <h1 className='text-xl font-mono cursor-pointer ml-5 text-cyan-400 bg-white py-2 px-6 rounded-2xl' onClick={()=>auth.signOut()}>SignOut</h1>
+                </div>
                 }
                 
             </div>
